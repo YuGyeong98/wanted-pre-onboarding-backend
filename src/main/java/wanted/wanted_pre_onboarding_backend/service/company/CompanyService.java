4c20e053ece460f3fda7b9b0da6accb761a4f382
YuGyeong98 <kuk3097@naver.com>
2024-07-28 17:12:56 +0900

    Feat: 회사 id가 없는 경우 예외 처리

--- src/main/java/wanted/wanted_pre_onboarding_backend/service/company/CompanyService.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/service/company/CompanyService.java
@@ -3,11 +3,14 @@ package wanted.wanted_pre_onboarding_backend.service.company;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import wanted.wanted_pre_onboarding_backend.common.CustomException;
 import wanted.wanted_pre_onboarding_backend.domain.Company;
 import wanted.wanted_pre_onboarding_backend.domain.Notice;
 import wanted.wanted_pre_onboarding_backend.repository.CompanyRepository;
 import wanted.wanted_pre_onboarding_backend.repository.NoticeRepository;
 import wanted.wanted_pre_onboarding_backend.service.company.dto.CreateNoticeDto;
+
+import static wanted.wanted_pre_onboarding_backend.common.ErrorCode.COMPANY_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class CompanyService {
     private final NoticeRepository noticeRepository;
 
     public Notice createNotice(Long id, CreateNoticeDto dto) {
-        Company company = companyRepository.findById(id).orElseThrow();
+        Company company = companyRepository.findById(id).orElseThrow(() -> new CustomException(COMPANY_NOT_FOUND));
         Notice notice = Notice.createNotice(company, dto.position(), dto.reward(), dto.content(), dto.techStack());
         return noticeRepository.save(notice);
     }
