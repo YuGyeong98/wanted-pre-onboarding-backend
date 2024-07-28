611c20b6a7f3be2e615142a4ad5968f9117f1015
YuGyeong98 <kuk3097@naver.com>
2024-07-28 17:31:34 +0900

    Feat: 회사 채용공고 등록 request에 validation 추가

--- src/main/java/wanted/wanted_pre_onboarding_backend/api/company/CompanyApiController.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/api/company/CompanyApiController.java
@@ -1,7 +1,9 @@
 package wanted.wanted_pre_onboarding_backend.api.company;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,12 +17,13 @@ import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class CompanyApiController {
 
     private final CompanyService companyService;
 
     @PostMapping("/api/notices/{id}")
-    public ResponseEntity<CreateNoticeResponse> createNotice(@PathVariable Long id, @RequestBody CreateNoticeRequest request) {
+    public ResponseEntity<CreateNoticeResponse> createNotice(@PathVariable Long id, @RequestBody @Valid CreateNoticeRequest request) {
         Notice notice = companyService.createNotice(id, request.toServiceDto());
         CreateNoticeResponse response = new CreateNoticeResponse(notice);
         return ResponseEntity.status(CREATED)
--- src/main/java/wanted/wanted_pre_onboarding_backend/api/company/request/CreateNoticeRequest.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/api/company/request/CreateNoticeRequest.java
@@ -1,14 +1,23 @@
 package wanted.wanted_pre_onboarding_backend.api.company.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import wanted.wanted_pre_onboarding_backend.service.company.dto.CreateNoticeDto;
 
 @Data
 public class CreateNoticeRequest {
 
+    @NotBlank(message = "채용포지션은 필수 입력입니다.")
     private String position;
-    private int reward;
+
+    @NotNull(message = "채용보상금은 필수 입력입니다.")
+    private Integer reward;
+
+    @NotBlank(message = "채용내용은 필수 입력입니다.")
     private String content;
+
+    @NotBlank(message = "사용기술은 필수 입력입니다.")
     private String techStack;
 
     public CreateNoticeDto toServiceDto() {
