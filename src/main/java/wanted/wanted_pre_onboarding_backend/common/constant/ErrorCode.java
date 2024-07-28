0e23698f9e293a66f6156848814d0ff47542e2c3
YuGyeong98 <kuk3097@naver.com>
2024-07-28 19:25:10 +0900

    Feat: 사용자 채용공고 지원 service 구현

--- src/main/java/wanted/wanted_pre_onboarding_backend/common/constant/ErrorCode.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/common/constant/ErrorCode.java
@@ -11,7 +11,8 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public enum ErrorCode {
 
     COMPANY_NOT_FOUND(NOT_FOUND, "해당 id를 가진 회사가 없습니다."),
-    NOTICE_NOT_FOUND(NOT_FOUND, "해당 id를 가진 채용공고가 없습니다.");
+    NOTICE_NOT_FOUND(NOT_FOUND, "해당 id를 가진 채용공고가 없습니다."),
+    USER_NOT_FOUND(NOT_FOUND, "해당 id를 가진 사용자가 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/wanted/wanted_pre_onboarding_backend/service/user/UserService.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/service/user/UserService.java
@@ -4,12 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import wanted.wanted_pre_onboarding_backend.common.exception.CustomException;
+import wanted.wanted_pre_onboarding_backend.domain.ApplyHistory;
 import wanted.wanted_pre_onboarding_backend.domain.Notice;
+import wanted.wanted_pre_onboarding_backend.domain.User;
+import wanted.wanted_pre_onboarding_backend.repository.ApplyHistoryRepository;
 import wanted.wanted_pre_onboarding_backend.repository.NoticeRepository;
+import wanted.wanted_pre_onboarding_backend.repository.UserRepository;
 
 import java.util.List;
 
 import static wanted.wanted_pre_onboarding_backend.common.constant.ErrorCode.NOTICE_NOT_FOUND;
+import static wanted.wanted_pre_onboarding_backend.common.constant.ErrorCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +22,8 @@ import static wanted.wanted_pre_onboarding_backend.common.constant.ErrorCode.NOT
 public class UserService {
 
     private final NoticeRepository noticeRepository;
+    private final UserRepository userRepository;
+    private final ApplyHistoryRepository applyHistoryRepository;
 
     /**
      * 채용공고 목록 조회
@@ -36,5 +43,21 @@ public class UserService {
      */
     public Notice findNotice(Long id) {
         return noticeRepository.findById(id).orElseThrow(() -> new CustomException(NOTICE_NOT_FOUND));
+    }
+
+    /**
+     * 채용공고 지원
+     *
+     * @param userId   사용자 id
+     * @param noticeId 채용공고 id
+     * @return 채용공고 지원 내역
+     * @throws CustomException USER_NOT_FOUND, NOTICE_NOT_FOUND
+     */
+    @Transactional
+    public ApplyHistory applyNotice(Long userId, Long noticeId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+        Notice notice = noticeRepository.findById(noticeId).orElseThrow(() -> new CustomException(NOTICE_NOT_FOUND));
+        ApplyHistory applyHistory = new ApplyHistory(user, notice);
+        return applyHistoryRepository.save(applyHistory);
     }
 }
