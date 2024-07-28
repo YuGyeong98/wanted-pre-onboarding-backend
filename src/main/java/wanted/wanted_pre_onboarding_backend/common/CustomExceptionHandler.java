4d6404f7f6f9e1f961f327552d9c4b706b489a9b
YuGyeong98 <kuk3097@naver.com>
2024-07-28 17:30:29 +0900

    Feat: handleMethodArgumentNotValid 예외처리 구현

--- src/main/java/wanted/wanted_pre_onboarding_backend/common/CustomExceptionHandler.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/common/CustomExceptionHandler.java
@@ -1,9 +1,17 @@
 package wanted.wanted_pre_onboarding_backend.common;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Objects;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -11,5 +19,13 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException customException) {
         return ErrorResponse.toResponseEntity(customException.getErrorCode());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String name = Objects.requireNonNull(ex.getFieldError()).getField().toUpperCase();
+        String message = ex.getFieldError().getDefaultMessage();
+        return ResponseEntity.status(BAD_REQUEST)
+                .body(new ErrorResponse(name, message));
     }
 }
