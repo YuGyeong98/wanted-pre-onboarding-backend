9933498bb5ead942ce3ef7d5cf308225802287fc
YuGyeong98 <kuk3097@naver.com>
2024-07-28 14:58:07 +0900

    Feat: 사용자 도메인 구현

---
+++ src/main/java/wanted/wanted_pre_onboarding_backend/domain/User.java
@@ -0,0 +1,15 @@
+package wanted.wanted_pre_onboarding_backend.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+}
