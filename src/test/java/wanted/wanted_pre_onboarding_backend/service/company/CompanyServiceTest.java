27f3e5f48a08e9addc671498c6410aaed4011e7d
YuGyeong98 <kuk3097@naver.com>
2024-07-29 00:16:26 +0900

    Test: 회사 채용공고 수정 기능 service 테스트 코드 구현

--- src/test/java/wanted/wanted_pre_onboarding_backend/service/company/CompanyServiceTest.java
+++ src/test/java/wanted/wanted_pre_onboarding_backend/service/company/CompanyServiceTest.java
@@ -12,6 +12,7 @@ import wanted.wanted_pre_onboarding_backend.domain.Notice;
 import wanted.wanted_pre_onboarding_backend.repository.CompanyRepository;
 import wanted.wanted_pre_onboarding_backend.repository.NoticeRepository;
 import wanted.wanted_pre_onboarding_backend.service.company.dto.CreateNoticeDto;
+import wanted.wanted_pre_onboarding_backend.service.company.dto.UpdateNoticeDto;
 
 import java.util.Optional;
 
@@ -20,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static wanted.wanted_pre_onboarding_backend.common.constant.ErrorCode.COMPANY_NOT_FOUND;
+import static wanted.wanted_pre_onboarding_backend.common.constant.ErrorCode.NOTICE_NOT_FOUND;
 
 @ExtendWith(MockitoExtension.class)
 class CompanyServiceTest {
@@ -66,5 +68,41 @@ class CompanyServiceTest {
 
         // then
         assertThat(exception.getErrorCode()).isEqualTo(COMPANY_NOT_FOUND);
+    }
+
+    @DisplayName("회사가 채용공고 수정을 하면 수정된 채용공고를 반환한다.")
+    @Test
+    void companyUpdateNoticeReturnUpdatedNotice() {
+        // given
+        Long id = 1L;
+        UpdateNoticeDto dto = new UpdateNoticeDto("프론트엔드 주니어 개발자", 1000000, "원티드랩에서 프론트엔드 주니어 개발자를 채용합니다. 자격요건은..", "Javascript");
+        Notice notice = new Notice("백엔드 주니어 개발자", 1000000, "원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..", "Python");
+
+        when(noticeRepository.findById(any())).thenReturn(Optional.of(notice));
+
+        // when
+        Notice result = companyService.updateNotice(id, dto);
+
+        // then
+        assertThat(result.getPosition()).isEqualTo(notice.getPosition());
+        assertThat(result.getReward()).isEqualTo(notice.getReward());
+        assertThat(result.getContent()).isEqualTo(notice.getContent());
+        assertThat(result.getTechStack()).isEqualTo(notice.getTechStack());
+    }
+
+    @DisplayName("등록되지 않은 채용공고를 수정하면 예외가 발생한다.")
+    @Test
+    void notFoundNoticeUpdateNoticeThrowsException() {
+        // given
+        Long id = 1L;
+        UpdateNoticeDto dto = new UpdateNoticeDto("프론트엔드 주니어 개발자", 1000000, "원티드랩에서 프론트엔드 주니어 개발자를 채용합니다. 자격요건은..", "Javascript");
+
+        when(noticeRepository.findById(any())).thenReturn(Optional.empty());
+
+        // when
+        CustomException exception = assertThrows(CustomException.class, () -> companyService.updateNotice(id, dto));
+
+        // then
+        assertThat(exception.getErrorCode()).isEqualTo(NOTICE_NOT_FOUND);
     }
 }
