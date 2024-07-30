c10ed016421098d8fa917c8fae8250ac8872c63e
YuGyeong98 <kuk3097@naver.com>
2024-07-31 02:42:04 +0900

    Refactor: SuccessResponse 삭제 및 ResponseEntity 메소드로 변경

--- src/main/java/wanted/wanted_pre_onboarding_backend/api/company/CompanyApiController.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/api/company/CompanyApiController.java
@@ -9,11 +9,10 @@ import wanted.wanted_pre_onboarding_backend.api.company.request.CreateNoticeRequ
 import wanted.wanted_pre_onboarding_backend.api.company.request.UpdateNoticeRequest;
 import wanted.wanted_pre_onboarding_backend.api.company.response.CreateNoticeResponse;
 import wanted.wanted_pre_onboarding_backend.api.company.response.UpdateNoticeResponse;
-import wanted.wanted_pre_onboarding_backend.common.response.SuccessResponse;
 import wanted.wanted_pre_onboarding_backend.domain.Notice;
 import wanted.wanted_pre_onboarding_backend.service.company.CompanyService;
 
-import static wanted.wanted_pre_onboarding_backend.common.constant.SuccessCode.*;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,10 +29,11 @@ public class CompanyApiController {
      * @return 201 CREATED, 400 BAD_REQUEST, 404 NOT_FOUND
      */
     @PostMapping("/api/notices/{companyId}")
-    public ResponseEntity<SuccessResponse<CreateNoticeResponse>> createNotice(@PathVariable Long companyId, @RequestBody @Valid CreateNoticeRequest request) {
+    public ResponseEntity<CreateNoticeResponse> createNotice(@PathVariable Long companyId, @RequestBody @Valid CreateNoticeRequest request) {
         Notice notice = companyService.createNotice(companyId, request.toServiceDto());
         CreateNoticeResponse response = new CreateNoticeResponse(notice);
-        return SuccessResponse.toResponseEntity(NOTICE_CREATED, response);
+        return ResponseEntity.status(CREATED)
+                .body(response);
     }
 
     /**
@@ -44,10 +44,10 @@ public class CompanyApiController {
      * @return 200 OK, 400 BAD_REQUEST, 404 NOT_FOUND
      */
     @PutMapping("/api/notices/{noticeId}")
-    public ResponseEntity<SuccessResponse<UpdateNoticeResponse>> updateNotice(@PathVariable Long noticeId, @RequestBody @Valid UpdateNoticeRequest request) {
+    public ResponseEntity<UpdateNoticeResponse> updateNotice(@PathVariable Long noticeId, @RequestBody @Valid UpdateNoticeRequest request) {
         Notice notice = companyService.updateNotice(noticeId, request.toServiceDto());
         UpdateNoticeResponse response = new UpdateNoticeResponse(notice);
-        return SuccessResponse.toResponseEntity(NOTICE_UPDATED, response);
+        return ResponseEntity.ok(response);
     }
 
     /**
@@ -57,8 +57,8 @@ public class CompanyApiController {
      * @return 200 OK, 404 NOT_FOUND
      */
     @DeleteMapping("/api/notices/{noticeId}")
-    public ResponseEntity<SuccessResponse<Object>> deleteNotice(@PathVariable Long noticeId) {
+    public ResponseEntity<String> deleteNotice(@PathVariable Long noticeId) {
         companyService.deleteNotice(noticeId);
-        return SuccessResponse.toResponseEntity(NOTICE_DELETED);
+        return ResponseEntity.ok("채용공고가 삭제되었습니다.");
     }
 }
--- src/main/java/wanted/wanted_pre_onboarding_backend/api/user/UserApiController.java
+++ src/main/java/wanted/wanted_pre_onboarding_backend/api/user/UserApiController.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.*;
 import wanted.wanted_pre_onboarding_backend.api.user.response.ApplyNoticeResponse;
 import wanted.wanted_pre_onboarding_backend.api.user.response.FindNoticeDetailResponse;
 import wanted.wanted_pre_onboarding_backend.api.user.response.FindNoticeResponse;
-import wanted.wanted_pre_onboarding_backend.common.response.SuccessResponse;
 import wanted.wanted_pre_onboarding_backend.domain.ApplyHistory;
 import wanted.wanted_pre_onboarding_backend.domain.Notice;
 import wanted.wanted_pre_onboarding_backend.service.user.UserService;
 
 import java.util.List;
 
-import static wanted.wanted_pre_onboarding_backend.common.constant.SuccessCode.*;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,10 +26,10 @@ public class UserApiController {
      * @return 200 OK
      */
     @GetMapping("/api/users/notices")
-    public ResponseEntity<SuccessResponse<List<FindNoticeResponse>>> findNotices() {
+    public ResponseEntity<List<FindNoticeResponse>> findNotices() {
         List<Notice> notices = userService.findNotices();
         List<FindNoticeResponse> response = notices.stream().map(FindNoticeResponse::new).toList();
-        return SuccessResponse.toResponseEntity(NOTICES_FOUND, response);
+        return ResponseEntity.ok(response);
     }
 
     /**
@@ -40,10 +39,10 @@ public class UserApiController {
      * @return 200 OK
      */
     @GetMapping("/api/users/notices/search")
-    public ResponseEntity<SuccessResponse<List<FindNoticeResponse>>> searchNotices(@RequestParam String searchCondition) {
+    public ResponseEntity<List<FindNoticeResponse>> searchNotices(@RequestParam String searchCondition) {
         List<Notice> notices = userService.searchNotices(searchCondition);
         List<FindNoticeResponse> response = notices.stream().map(FindNoticeResponse::new).toList();
-        return SuccessResponse.toResponseEntity(NOTICE_SEARCH_FOUND, response);
+        return ResponseEntity.ok(response);
     }
 
     /**
@@ -53,10 +52,10 @@ public class UserApiController {
      * @return 200 OK, 404 NOT_FOUND
      */
     @GetMapping("/api/users/notices/{noticeId}")
-    public ResponseEntity<SuccessResponse<FindNoticeDetailResponse>> findNotice(@PathVariable Long noticeId) {
+    public ResponseEntity<FindNoticeDetailResponse> findNotice(@PathVariable Long noticeId) {
         Notice notice = userService.findNotice(noticeId);
         FindNoticeDetailResponse response = new FindNoticeDetailResponse(notice);
-        return SuccessResponse.toResponseEntity(NOTICE_FOUND, response);
+        return ResponseEntity.ok(response);
     }
 
     /**
@@ -67,9 +66,10 @@ public class UserApiController {
      * @return 200 OK, 404 NOT_FOUND
      */
     @PostMapping("/api/users/{userId}/notices/{noticeId}")
-    public ResponseEntity<SuccessResponse<ApplyNoticeResponse>> applyNotice(@PathVariable Long userId, @PathVariable Long noticeId) {
+    public ResponseEntity<ApplyNoticeResponse> applyNotice(@PathVariable Long userId, @PathVariable Long noticeId) {
         ApplyHistory applyHistory = userService.applyNotice(userId, noticeId);
         ApplyNoticeResponse response = new ApplyNoticeResponse(applyHistory);
-        return SuccessResponse.toResponseEntity(NOTICE_APPLIED, response);
+        return ResponseEntity.status(CREATED)
+                .body(response);
     }
 }
